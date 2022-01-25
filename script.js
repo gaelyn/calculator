bb9a24de7db0547e2645ab3ba2ceb2eaf508984a
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2022-01-25 11:38:53 -0600

    Compute function

--- script.js
+++ script.js
@@ -31,7 +31,30 @@ class Calculator {
   }
   
   compute() {
+    let computation
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    if (isNan(prev) || isNan(current)) return
+    switch (this.operation) {
+      case '+':
+        computation = prev + current
+        break
+      case '-':
+        computation = prev - current
+        break
+      case '*':
+        computation = prev * current
+        break
+      case '÷':
+        computation = prev / current
+        break
+      default: 
+        return
+    }
     
+    this.currentOperand = computation
+    this.operation = undefined
+    this.previousOperand = ''
   }
   
   updateDisplay() {
@@ -62,4 +85,9 @@ operationButtons.forEach(button => {
     calculator.chooseOperation(button.innerText)
     calculator.updateDisplay()
   })
+})
+
+equalsButton.addEventListener('click', button => {
+  calculator.compute()
+  calculator.updateDisplay()
 })
